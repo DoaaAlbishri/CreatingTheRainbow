e8eb02548422df1e4d8acce608fbe51e4a40b481
Doaa <Duaa_ali@outlook.sa>
2021-11-08 13:20:54 +0300

    second commit

--- lib/MyHomePage.dart
+++ lib/MyHomePage.dart
@@ -1,62 +1,70 @@
-
-
 import 'package:flutter/material.dart';
 
 class MyHomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    double screenHeight = MediaQuery.of(context).size.height;
+    double appBarHeight = MediaQuery.of(context).padding.top + kToolbarHeight;
+    double heightBox = screenHeight - appBarHeight;
+
     return Scaffold(
       appBar: AppBar(
         title: Text("Rainbow Layout"),
+
       ),
       body: Container(
-          child: Column(
-            children: [
+          child: RainbowLayout(boxHeight: heightBox)
+      ),
+    );
+  }
+}
+
+class RainbowLayout extends StatelessWidget {
+  const RainbowLayout({Key? key, required this.boxHeight,}) : super(key: key);
+  final double boxHeight;
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
               Container(
                 color: Colors.red,
                 child: SizedBox(
-                  height:100,
-                  width: 100
+                  height:boxHeight/6
                 ),
               ),
               Container(
                 color: Colors.orange,
                 child: SizedBox(
-                    height:100,
-                  width: 100
+                    height:boxHeight/6
                 ),
               ),
               Container(
                 color: Colors.yellow,
                 child: SizedBox(
-                    height:100,
-                    width: 100
+                    height:boxHeight/6
                 ),
               ),
               Container(
                 color: Colors.green,
                 child: SizedBox(
-                    height:100,
-                    width: 100
+                    height:boxHeight/6
                 ),
               ),
               Container(
                 color: Colors.blue,
                 child: SizedBox(
-                    height:100,
-                    width: 100
+                    height:boxHeight/6
                 ),
               ),
               Container(
                 color: Colors.purple,
                 child: SizedBox(
-                    height:100,
-                    width: 100
+                    height:boxHeight/6
                 ),
               )
             ],
-          )
-      ),
-    );
+          );
   }
 }
